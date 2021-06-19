d4d81b91e0dac05ca985bfc698fa61490e11e650
Ahmed Habeeb <ahmedhabeeb@Ahmeds-Mac-mini.fritz.box>
2021-06-19 22:09:15 +0200

    filtering off the end of a day

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -5,14 +5,16 @@ use App\Models\Schedule;
 use Carbon\CarbonInterval;
 use Illuminate\Http\Request;
 use App\Bookings\TimeSlotGenerator;
+use App\Models\Service;
 
 class BookingController extends Controller
 {
     public function __invoke()
     {
         $schedule=Schedule::find(1);
+        $service=Service::find(1);
         
-        $slots=(new TimeSlotGenerator($schedule))->get();
+        $slots=(new TimeSlotGenerator($schedule,$service))->get();
         return view('bookings.create',[
             'slots'=>$slots
         ]);
--- app/bookings/TimeSlotGenerator.php
+++ app/bookings/TimeSlotGenerator.php
@@ -2,6 +2,7 @@
 
 namespace App\Bookings;
 use App\Models\Schedule;
+use App\Models\Service;
 use Carbon\CarbonInterval;
 
 
@@ -9,13 +10,15 @@ class TimeSlotGenerator
 {
   public const INCREMENT=15;
 protected $interval;
-  public function __construct(Schedule $schedule)
+  public function __construct(Schedule $schedule,Service $service)
   {
 
     $this->interval=CarbonInterval::minutes(self::INCREMENT)
     ->toPeriod(
         $schedule->date->setTimeFrom($schedule->start_time),
-        $schedule->date->setTimeFrom($schedule->end_time)
+        $schedule->date->setTimeFrom(
+          $schedule->end_time->subMinutes($service->duration)
+          )
     );
 
   }
